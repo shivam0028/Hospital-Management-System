714c189bf17069c600827c28dce5d9d4bc8c89b3
shivam0028 <shivamk0117t@gmail.com>
2026-04-13 22:15:23 +0530

    updated port and host in pythonn

--- backend/app.py
+++ backend/app.py
@@ -300,4 +300,5 @@ def delete_appointment(patient_id: str):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.getenv('PORT', '5000'))
+    app.run(host='0.0.0.0', port=port)
